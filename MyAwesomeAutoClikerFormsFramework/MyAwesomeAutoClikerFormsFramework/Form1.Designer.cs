141e31010282aae9189545b9b8d194014a9bb549
mathias brink <mathiasbrink@live.dk>
2020-09-10 23:25:16 +0200

    added click on key press and different interval
    There is now hour, minute, second and milisecond intervals and Start/Stop Click on key pressed

--- MyAwesomeAutoClikerFormsFramework/MyAwesomeAutoClikerFormsFramework/Form1.Designer.cs
+++ MyAwesomeAutoClikerFormsFramework/MyAwesomeAutoClikerFormsFramework/Form1.Designer.cs
@@ -28,63 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.components = new System.ComponentModel.Container();
+            this.textBoxTimer = new System.Windows.Forms.TextBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMinuter = new System.Windows.Forms.TextBox();
+            this.textBoxSekunder = new System.Windows.Forms.TextBox();
+            this.textBoxMilisekunder = new System.Windows.Forms.TextBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxTimer
             // 
-            this.textBox1.Location = new System.Drawing.Point(49, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(93, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Timer";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(49, 176);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(59, 17);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Enable";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.textBoxTimer.Location = new System.Drawing.Point(49, 45);
+            this.textBoxTimer.Name = "textBoxTimer";
+            this.textBoxTimer.Size = new System.Drawing.Size(93, 20);
+            this.textBoxTimer.TabIndex = 0;
+            this.textBoxTimer.Text = "0";
             // 
             // backgroundWorker1
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(161, 45);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(168, 43);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Set";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 26);
+            this.label1.Location = new System.Drawing.Point(46, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Interval";
             // 
+            // textBoxMinuter
+            // 
+            this.textBoxMinuter.Location = new System.Drawing.Point(148, 45);
+            this.textBoxMinuter.Name = "textBoxMinuter";
+            this.textBoxMinuter.Size = new System.Drawing.Size(93, 20);
+            this.textBoxMinuter.TabIndex = 9;
+            this.textBoxMinuter.Text = "0";
+            // 
+            // textBoxSekunder
+            // 
+            this.textBoxSekunder.Location = new System.Drawing.Point(247, 45);
+            this.textBoxSekunder.Name = "textBoxSekunder";
+            this.textBoxSekunder.Size = new System.Drawing.Size(93, 20);
+            this.textBoxSekunder.TabIndex = 10;
+            this.textBoxSekunder.Text = "0";
+            // 
+            // textBoxMilisekunder
+            // 
+            this.textBoxMilisekunder.Location = new System.Drawing.Point(346, 45);
+            this.textBoxMilisekunder.Name = "textBoxMilisekunder";
+            this.textBoxMilisekunder.Size = new System.Drawing.Size(93, 20);
+            this.textBoxMilisekunder.TabIndex = 11;
+            this.textBoxMilisekunder.Text = "0";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(247, 246);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(192, 43);
+            this.buttonStop.TabIndex = 12;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Visible = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Timer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Minuter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Sekunder";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Milisekunder";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(49, 246);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(192, 43);
+            this.buttonStart.TabIndex = 17;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 394);
+            this.ClientSize = new System.Drawing.Size(507, 388);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.textBoxMilisekunder);
+            this.Controls.Add(this.textBoxSekunder);
+            this.Controls.Add(this.textBoxMinuter);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxTimer);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -95,11 +176,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBoxTimer;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxMinuter;
+        private System.Windows.Forms.TextBox textBoxSekunder;
+        private System.Windows.Forms.TextBox textBoxMilisekunder;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- MyAwesomeAutoClikerFormsFramework/MyAwesomeAutoClikerFormsFramework/Form1.cs
+++ MyAwesomeAutoClikerFormsFramework/MyAwesomeAutoClikerFormsFramework/Form1.cs
@@ -22,9 +22,18 @@ namespace MyAwesomeAutoClikerFormsFramework
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
         private const int leftUp = 0x0004;
         private const int leftDown = 0x0002;
+        private int hour;
+        private int minutes;
+        private int seconds;
+        private int milisconds;
         public int intervals = 5;
         public int parsedValue;
         public bool click = false;
+
+        public int Hour { get => hour; set => hour = value; }
+        public int Minutes { get => minutes; set => minutes = value; }
+        public int Seconds { get => seconds; set => seconds = value; }
+        public int Milisconds { get => milisconds; set => milisconds = value; }
 
         public Form1()
         {
@@ -59,32 +68,73 @@ namespace MyAwesomeAutoClikerFormsFramework
         {
             while (true)
             {
-                if (checkBox1.Checked)
+
+                if (!buttonStart.Visible)
                 {
-                    if (GetAsyncKeyState(Keys.Down) < 0)
-                    {
-                        click = false;
-                    }
-                    else if (GetAsyncKeyState(Keys.Up) < 0)
-                    {
-                        click = true;
-                    }
+                    click = true;
                     Thread.Sleep(1);
                 }
+                else if (buttonStart.Visible)
+                {
+                    click = false;
+                    Thread.Sleep(1);
+                }
+                if (GetAsyncKeyState(Keys.Up) < 0)
+                {
+                    buttonStart.Visible = false;
+                    buttonStop.Visible = true;
+                    click = true;
+                    Thread.Sleep(1);
+                }
+                if (GetAsyncKeyState(Keys.Down) < 0)
+                {
+                    buttonStart.Visible = true;
+                    buttonStop.Visible = false;
+                    click = false;
+                    Thread.Sleep(1);
+                }
+
+                
                 Thread.Sleep(1);
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void buttonStart_Click(object sender, EventArgs e)
         {
-            if (!int.TryParse(textBox1.Text, out parsedValue))
+            if (!int.TryParse(textBoxMilisekunder.Text, out parsedValue) ||
+                !int.TryParse(textBoxSekunder.Text, out parsedValue) ||
+                !int.TryParse(textBoxMinuter.Text, out parsedValue) ||
+                !int.TryParse(textBoxTimer.Text, out parsedValue))
             {
-                MessageBox.Show("indtast et tal");
+                MessageBox.Show("indtast venligst et tal");
                 return;
-            } else
-            {
-                intervals = int.Parse(textBox1.Text);
             }
+            else
+            {
+                TimeConverter();
+                intervals = Hour + Minutes + Seconds + Milisconds;
+                buttonStart.Visible = false;
+                buttonStop.Visible = true;
+            }
+        }
+
+        private void TimeConverter()
+        {
+            Hour = Convert.ToInt32(textBoxTimer.Text) * 3600000;
+            Minutes = Convert.ToInt32(textBoxMinuter.Text) * 60000;
+            Seconds = Convert.ToInt32(textBoxSekunder.Text) * 1000;
+            Milisconds = Convert.ToInt32(textBoxMilisekunder.Text);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonStart.Visible = true;
+            buttonStop.Visible = false;
         }
     }
 }
